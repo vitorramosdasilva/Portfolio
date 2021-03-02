af71874e789a8a82131d00a27c0510edc27a718b
vitorramosdasilva <rsvitor_81@hotmail.com>
2021-03-02 20:03:30 -0300

    primeiro_heroku

--- my_proj/urls.py
+++ my_proj/urls.py
@@ -13,9 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
+    path('', include('portfolio.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler50x = 'portfolio.views.error_500'
+handler404 = 'portfolio.views.error_404'
+handler403 = 'portfolio.views.error_403'
+handler400 = 'portfolio.views.error_400'
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,51 @@
+# coding=utf-8
+from django.contrib import messages
+from django.core.mail import send_mail, BadHeaderError
+from portfolio.forms import ContatoForm
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+
+def IndexView(request):
+    if request.method == 'GET':
+        form = ContatoForm()
+        return render(request, "index.html", {'form': form})
+    else:
+        form = ContatoForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            message = 'Assunto: ' + subject + '\n' + 'Name: ' + name + '\n' + 'Email: ' + email + '\n' + 'Messagem: ' + message
+            messages.success(request, 'E-mail enviado com sucesso.\nRetornaremos em até 48 horas úteis.')
+            try:
+                send_mail(subject,
+                          message,
+                          settings.EMAIL_HOST_USER,
+                          [settings.EMAIL_HOST_USER],
+                          fail_silently=False
+                          )
+
+                form = ContatoForm
+            except BadHeaderError:
+                messages.error(request, 'Erro ao enviar e-mail.\nTente Novamente e revise o conteúdo.')
+            return render(request, "index.html", {'form': form})
+    return render(request, "index.html", {'form': form})
+
+
+# Page Error ...
+def error_400(request, exception):
+    return render(request, '400.html', status=400)
+
+
+def error_403(request, exception):
+    return render(request, '403.html', status=403)
+
+
+def error_404(request, exception):
+    return render(request, '404.html', status=404)
+
+
+def error_50x(request, exception):
+    return render(request, '500.html', status=500)
